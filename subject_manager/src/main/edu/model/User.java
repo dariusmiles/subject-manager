4643b0cb7466324cfe9daa477e4de5b4422cc036
karl alejandro <karlbarth.alejandro@yahoo.com>
2013-05-29 03:25:57 +0800

    added class User

---
+++ subject_manager/src/main/edu/model/User.java
@@ -0,0 +1,56 @@
+package main.edu.model;
+
+public class User {
+	private String username;
+	private String password;
+	private String role;
+	private String email;
+	private String firstname;
+	private String lastname;
+	
+	
+	
+	public User() {
+		super();
+		// TODO Auto-generated constructor stub
+	}
+	public String getUsername() {
+		return username;
+	}
+	public void setUsername(String username) {
+		this.username = username;
+	}
+	public String getPassword() {
+		return password;
+	}
+	public void setPassword(String password) {
+		this.password = password;
+	}
+	public String getRole() {
+		return role;
+	}
+	public void setRole(String role) {
+		this.role = role;
+	}
+	public String getEmail() {
+		return email;
+	}
+	public void setEmail(String email) {
+		this.email = email;
+	}
+	public String getFirstname() {
+		return firstname;
+	}
+	public void setFirstname(String firstname) {
+		this.firstname = firstname;
+	}
+	public String getLastname() {
+		return lastname;
+	}
+	public void setLastname(String lastname) {
+		this.lastname = lastname;
+	}
+	
+	
+
+}
